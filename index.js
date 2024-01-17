c1c26808bef3d5e612b3bcab028f315662d1a4aa
Adarsh-shakya <adarsh.shakya_cs20@gla.ac.in>
2024-01-17 21:01:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ import cors from 'cors';
 
 const app = express();
 dotenv.config();
+
+
+
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://instgram-delta.vercel.app');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+
 app.use(cors(
      {
   origin : 'https://instgram-delta.vercel.app',
